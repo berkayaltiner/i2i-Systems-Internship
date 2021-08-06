cee6e16ced273d6a485d4b966e6504c874d01fd9
berkayaltiner <berkay.altiner.98@gmail.com>
2021-08-06 10:00:19 +0300

    Compress log files according to size and deleting by number

---
+++ com.log4j-LogCompression (03.08.2021)/src/test/java/com/Log4jtest/com/Log4jTest/AppTest.java
@@ -0,0 +1,20 @@
+package com.Log4jtest.com.Log4jTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
